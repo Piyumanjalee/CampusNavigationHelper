c66345bb045b0896bf17ac1070714f7cc407161b
imadh <imadhahmeth08@gmail.com>
2026-02-23 10:40:54 +0530

    Set click listener for Logout

--- app/src/main/java/com/example/campusnavigationhelper/MainActivity.java
+++ app/src/main/java/com/example/campusnavigationhelper/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Set click listener for Logout
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
